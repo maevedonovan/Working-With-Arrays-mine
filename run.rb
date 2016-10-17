9929e44db5c1d86243c349fa0398e9aeba9a65fc
Jonathan Arp <jarp@nd.edu>
2016-10-17 14:06:00 -0400

    comments

--- run.rb
+++ run.rb
@@ -9,7 +9,14 @@ end
 
 get '/years' do
   @years_Alive = 'I have been alive since 1995. Below are all the years that have passed since my birth.'
+  # looking for:
+    @years = Array(1995..2015)
+  #or
+    @years = (1995..2015).to_a
+
+  #entering each year individually should make you think, "Is there a better way to do this?"
   @years = [1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015]
+
 
   # code!
   erb :years, layout: :main
